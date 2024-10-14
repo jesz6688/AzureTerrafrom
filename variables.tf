a30471b3c03d9c1f244c960977b0a72882144fef
jesz6688 <85899322+jesz6688@users.noreply.github.com>
2024-10-14 11:35:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "resource_group_name" {
   type = string
-  default = "1-fb4b4b74-playground-sandbox"
+  default = "1-f39600b5-playground-sandbox"
 }
 
 variable "location" {
   type = string
-  default = "WEST US"
+  default = "southcentralus"
 }
