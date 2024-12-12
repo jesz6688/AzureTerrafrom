63a154822616e0fef67d04cb0b8c242151a11342
jesz6688 <85899322+jesz6688@users.noreply.github.com>
2024-12-12 16:01:13 +0530

    Update AzureCloudProviders.tf

--- AzureCloudProviders.tf
+++ AzureCloudProviders.tf
@@ -2,16 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.5.0"
+      version = "4.13.0"
     }
   }
 }
 
 provider "azurerm" {
-  # Configuration options chsnge
-  /*
-    Multi line comment
-  */
+  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
-  skip_provider_registration = true
 }
